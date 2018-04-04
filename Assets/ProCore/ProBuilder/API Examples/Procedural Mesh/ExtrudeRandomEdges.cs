9cf254e6793fa9b7ec3628a139f32b85047b1aba
Joel Lee <lee.joel395@gmail.com>
2018-04-03 18:30:49 -0700

    Probuilder Update

--- Assets/ProCore/ProBuilder/API Examples/Procedural Mesh/ExtrudeRandomEdges.cs
+++ Assets/ProCore/ProBuilder/API Examples/Procedural Mesh/ExtrudeRandomEdges.cs
@@ -1,4 +1,5 @@
-#define PROTOTYPE
+﻿#if UNITY_EDITOR || UNITY_STANDALONE
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -20,7 +21,7 @@ public class ExtrudeRandomEdges : MonoBehaviour
 	 */
 	void Start()
 	{
-		pb = pb_ShapeGenerator.PlaneGenerator(1, 1, 0, 0, Axis.Up, false);
+		pb = pb_ShapeGenerator.PlaneGenerator(1, 1, 0, 0, ProBuilder2.Common.Axis.Up, false);
 		pb.SetFaceMaterial(pb.faces, pb_Constant.DefaultMaterial);
 		lastExtrudedFace = pb.faces[0];
 	}
@@ -70,3 +71,4 @@ public class ExtrudeRandomEdges : MonoBehaviour
 		pb.Refresh();
 	}
 }
+#endif
