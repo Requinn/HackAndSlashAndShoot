7531fde1e88235e48765f5238967e39f432e0d60
Joel Lee <lee.joel395@gmail.com>
2018-04-14 00:36:35 -0700

    Gave damage zones the ability to activate on a timer updated the player prefab to use the new animations n shit

--- Assets/_Script/Enemies/DamageZone.cs
+++ Assets/_Script/Enemies/DamageZone.cs
@@ -6,12 +6,69 @@ using UnityEngine;
 public class DamageZone : MonoBehaviour {
     public bool CanAffectPlayer = true;
     public bool CanAffectEnemy = false;
+    public bool AlwaysActive = true;
+
+    [SerializeField]
+    private float _activeCooldown = 5.0f;
+    [SerializeField]
+    private float _warningDuration = 2.5f;
+    [SerializeField]
+    private float _activeDuration = 3.0f;
+
     public float TickTime = 3.0f;
     public float Damage = 15.0f;
+
     [SerializeField]
     public Damage.DamageType type = JLProject.Damage.DamageType.Neutral;
 
     private bool _canTick = true;
+
+    private BoxCollider _collider;
+    private Renderer _renderer;
+
+    void Start(){
+        _collider = GetComponent<BoxCollider>();
+        _renderer = GetComponent<Renderer>();
+        if (!AlwaysActive){
+            Timing.RunCoroutine(CycleActivation());
+            _collider.enabled = false;
+            _renderer.material.color = Color.white;
+        }
+        if (AlwaysActive){
+            _renderer.material.color = Color.red;
+        }
+        
+    }
+
+    private IEnumerator<float> CycleActivation(){
+        while (true){
+            yield return Timing.WaitForSeconds(_activeCooldown);
+            InvokeRepeating("FlashColor", 0f, 0.2f);
+            yield return Timing.WaitForSeconds(_warningDuration);
+            CancelInvoke("FlashColor");
+            ActivateDamage();
+            yield return Timing.WaitForSeconds(_activeDuration);
+            DeactivateDamage();
+        }
+    }
+
+    private void FlashColor(){
+        if (_renderer.material.color != Color.white){
+            _renderer.material.color = Color.white;
+        }else if (_renderer.material.color != Color.gray){
+            _renderer.material.color = Color.grey;
+        }
+    }
+
+    private void ActivateDamage(){
+        _renderer.material.color = Color.red;
+        _collider.enabled = true;
+    }
+
+    private void DeactivateDamage(){
+        _renderer.material.color = Color.white;
+        _collider.enabled = false;
+    }
 
     /// <summary>
     /// weak Heal method, expand to maybe heal enemies as well
