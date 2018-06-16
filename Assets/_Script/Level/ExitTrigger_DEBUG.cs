8ff060ffbb35649811c5a4a342157c1a5d90a6f0
Joel Lee <lee.joel395@gmail.com>
2018-06-16 13:14:26 -0700

    Quick bugfixes for public build

---
+++ Assets/_Script/Level/ExitTrigger_DEBUG.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+namespace JLProject {
+    /// <summary>
+    /// transition to the next level
+    /// </summary>
+    public class ExitTrigger_DEBUG : MonoBehaviour{
+        public int LevelToLoad;
+        void OnTriggerEnter(Collider c) {
+            if (c.tag == "Player") {
+                SceneLoader.Instance.LoadLevel(LevelToLoad);
+            }
+        }
+    }
+}
