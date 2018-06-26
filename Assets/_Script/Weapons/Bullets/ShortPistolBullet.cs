8fee3ba79be357225e1f3b26cad468ff5f6ba31f
Joel Lee <lee.joel395@gmail.com>
2018-06-25 19:09:24 -0700

    Added better collision for bullets to support high velocities. Created a sniper enemy with a new cloaking ability. Sniper will aim at the player then fire, cloaking and running away if they get too close.

--- Assets/_Script/Weapons/Bullets/ShortPistolBullet.cs
+++ Assets/_Script/Weapons/Bullets/ShortPistolBullet.cs
@@ -13,6 +13,8 @@ public class ShortPistolBullet : MonoBehaviour, IProjectile{
     private float _lifetime;
     public StatusObject statusObj;
     private CoroutineHandle _repoolHandle;
+    private RaycastHit hit;
+    private Ray ray;
 
     public void OnEnable(){
         _curPos = transform.position;
@@ -20,6 +22,19 @@ public class ShortPistolBullet : MonoBehaviour, IProjectile{
         _repoolHandle = Timing.RunCoroutine(DelayedRepool(_lifetime));
     }
 
+    //Raycast ahead for high velocity bullets
+    void FixedUpdate(){
+        ray = new Ray(transform.position, (transform.position - _curPos).normalized);
+        Physics.Raycast(ray, out hit, 5f);
+        if (hit.transform){
+            DoCollisionCheck(hit.transform.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// how long we should live before being repooled
+    /// </summary>
+    /// <returns></returns>
     private float CalculateTimeToLive(){
         return FlightDistance / Velocity;
     }
@@ -28,16 +43,20 @@ public class ShortPistolBullet : MonoBehaviour, IProjectile{
         args.SourceFaction = f;
     }
 
-    public void OnTriggerEnter(Collider c){
-        Entity ent = c.GetComponent<Entity>();
-        Switch swtch = c.GetComponent<Switch>();
-        BreakableObject brk = c.GetComponent<BreakableObject>();
-        if (ent != null){
-            if (ent.Faction != Damage.Faction.Player || ent.Faction != Damage.Faction.Allied){
+    /// <summary>
+    /// Check for collision
+    /// </summary>
+    /// <param name="hitObject"></param>
+    void DoCollisionCheck(GameObject hitObject){
+        Entity ent = hitObject.GetComponent<Entity>();
+        Switch swtch = hitObject.GetComponent<Switch>();
+        BreakableObject brk = hitObject.GetComponent<BreakableObject>();
+        if (ent != null) {
+            if (ent.Faction != Damage.Faction.Player || ent.Faction != Damage.Faction.Allied) {
                 //fix this later
                 args.HitSourceLocation = transform.position;
                 ent.TakeDamage(gameObject, ref args);
-                if (statusObj){
+                if (statusObj) {
                     ApplyStatus(statusObj, ent);
                 }
                 Timing.KillCoroutines(_repoolHandle);
@@ -45,24 +64,32 @@ public class ShortPistolBullet : MonoBehaviour, IProjectile{
             }
             StopCoroutine(DelayedRepool(_lifetime));
         }
-        else if (c.gameObject.tag == "Environment"){
+        else if (hitObject.gameObject.tag == "Environment") {
             Timing.KillCoroutines(_repoolHandle);
             gameObject.SetActive(false);
         }
         if (args.SourceFaction != Damage.Faction.Player) return;
-        if (swtch){
+        if (swtch) {
             swtch.Toggle();
             Timing.KillCoroutines(_repoolHandle);
             gameObject.SetActive(false);
         }
-        if (brk){
+        if (brk) {
             brk.GetComponent<BreakableObject>().Hit();
             Timing.KillCoroutines(_repoolHandle);
             gameObject.SetActive(false);
         }
-
     }
 
+    public void OnTriggerEnter(Collider c){
+       DoCollisionCheck(c.gameObject);
+    }
+
+    /// <summary>
+    /// repool bullet if its life is up
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
     private IEnumerator<float> DelayedRepool(float t){
         yield return Timing.WaitForSeconds(t);
         Timing.KillCoroutines(_repoolHandle);
