2311b7a0393c29f4db58d05f83c948eed3c55fdd
Joel Lee <lee.joel395@gmail.com>
2018-07-02 16:39:22 -0700

    Finished sweeping laser attack

--- Assets/_Script/Enemies/AI/Bosses/MortarBoss/SweepingLaser.cs
+++ Assets/_Script/Enemies/AI/Bosses/MortarBoss/SweepingLaser.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using HutongGames.PlayMaker.Actions;
 using JLProject;
 using UnityEngine;
 using MEC;
+using ProBuilder2.Common;
 
 public class SweepingLaser : MonoBehaviour{
     //only need to use single floats as we rotate only on the Y plane
@@ -11,7 +13,6 @@ public class SweepingLaser : MonoBehaviour{
     public float sweepAngle = 180f; //how much of an angle do we move
     public float timeToSweep = 7f; //how long it takes to finish the sweep
     public float damageTickRate = 3f; //how often we can deal damage
-    public bool canReflect = false; //does the laser bounce?
     public float reach = 30f;
     private bool _isDoneSweeping = false;
     private float _curSweepTime = 0f;
@@ -20,9 +21,9 @@ public class SweepingLaser : MonoBehaviour{
     private LineRenderer _lineRender;
     private Vector3 _origin;
     private Ray _ray;
-    [SerializeField]
-    private RaycastHit[] _hits;
 
+    private RaycastHit _hit;
+    private RaycastHit _playerHit;
     private Damage.DamageEventArgs args;
 
     void Start(){
@@ -37,7 +38,8 @@ public class SweepingLaser : MonoBehaviour{
         if (_timeSinceLastDamageTick < damageTickRate){
             _timeSinceLastDamageTick += Time.deltaTime;
         }
-   
+
+        CalculateHit();
         if (!_isDoneSweeping){
             _curSweepTime += Time.deltaTime;
             transform.DORotate(new Vector3(0, sweepAngle + initAngle, 0), timeToSweep).SetEase(Ease.Linear);
@@ -46,41 +48,72 @@ public class SweepingLaser : MonoBehaviour{
                 _isDoneSweeping = true;
             }
         }
-        CalculateHit();
+        
     }
 
-    public void StartSweep(){
+    public void StartSweep(float startAngle = 0f, float sweepAmount = 90f){
         _isDoneSweeping = false;
+        initAngle = startAngle;
+        sweepAngle = sweepAmount;
         _curSweepTime = 0f;
     }
+
+    /*
+    //if player collides with the laser hitbox, and is hittable, then do damage.
+    //using on trigger stay so the player can take damage while sitting in the laser
+    void OnTriggerStay(Collider c){
+        PlayerController p = c.GetComponent<PlayerController>();
+        if (p){
+            //check a straight ray from source to player, as using the forward will create a form of race condition where by the time you check
+            //the player's position, the forwards is somewhere else
+            if (Physics.Raycast(new Ray(transform.position, (p.transform.position - transform.position).normalized), out _playerHit, reach)){
+                if (_timeSinceLastDamageTick >= damageTickRate && _playerHit.transform.CompareTag("Player")){
+                    p.TakeDamage(this, ref args);
+                    _timeSinceLastDamageTick = 0;
+                }
+            }
+        }
+    }*/
 
     /// <summary>
     /// draw a laserfrom self to end of raycast
     /// </summary>
     void CalculateHit(){
-        _lineRender.positionCount = 2;
-        
+        _lineRender.SetPosition(0, _origin);
+
         _ray.origin = transform.position;
         _ray.direction = transform.forward;
-        _lineRender.SetPosition(0, _origin);
-        _lineRender.SetPosition(1, _origin + _ray.direction * 30f);
-
-        _hits = Physics.RaycastAll(_ray, reach);
 
         //inconsistent hit detection on the player
-        foreach (var v in _hits){
-            Debug.Log(v.transform.name);
-            Transform hitComponent = v.transform;
-            if (hitComponent.CompareTag("Environment")){
-                _lineRender.SetPosition(1, v.point);
-                break;
-            }
-            if (_timeSinceLastDamageTick >= damageTickRate && hitComponent.CompareTag("Player")){
-                
+        //inconsistent wall detection now?
+        //used to render the laser's end point using an array to render through enemies/player
+        if (Physics.Raycast(_ray, out _hit, reach)){
+            Transform hitComponent = _hit.transform;
+            _lineRender.positionCount = 2;
+            _lineRender.SetPosition(1, _hit.point);
+            //if (hitComponent.CompareTag("Environment")){ //a redundancy, maybe used to make it so the laser will pass through the player
+            //    _lineRender.SetPosition(1, _hit.point);
+            //}
+            if (_timeSinceLastDamageTick >= damageTickRate && hitComponent.transform.CompareTag("Player")){
                 hitComponent.GetComponent<PlayerController>().TakeDamage(this, ref args);
                 _timeSinceLastDamageTick = 0;
-                break;
             }
         }
+        else{
+            _lineRender.positionCount = 2;
+            _lineRender.SetPosition(1, _origin + _ray.direction * reach);
+        }
+        /**
+        foreach (var v in _hits){
+            Transform hitComponent = v.transform;
+
+            if (hitComponent.CompareTag("Environment")){
+                _lineRender.SetPosition(1, v.point);
+                if (hitComponent.name == "Barrier"){
+                    Debug.Log("Blocked");
+                }
+                break;
+            }
+        }*/
     }
 }
