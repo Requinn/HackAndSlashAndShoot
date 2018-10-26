45f5dd7ad45a32a872876bcf39ea11b00d5d3a2f
Joel Lee <lee.joel395@gmail.com>
2018-10-26 15:59:48 -0700

    Fixed an issue of a boss trying to attack while dead. Added in health bar lerping, where there taking damage will drop the health bar, but then also have a secondary bar show the original health that will drop over time to the current health.

--- Assets/_Script/Enemies/AI/Bosses/MortarBoss/BulletHell.cs
+++ Assets/_Script/Enemies/AI/Bosses/MortarBoss/BulletHell.cs
@@ -54,16 +54,18 @@ public class BulletHell : MonoBehaviour{
     IEnumerator<float> FirePulse(){
         isFiring = true;
         for (int j = 0; j < fireCount; j++) {
-            curAngle = minAngle;
-            for (int i = 0; i <= projectileCount; i++) { //add +1 to make the pattern symmetrical??
-                //Debug.Log(curAngle);
-                GameObject go = Instantiate(projectile, transform.position,
-                    Quaternion.Euler(0, transform.rotation.eulerAngles.y + curAngle, 0));
-                go.GetComponent<Rigidbody>().velocity = go.transform.forward * 5f;
-                Destroy(go, 5f);
-                curAngle += angleStep;
+            if (this) {
+                curAngle = minAngle;
+                for (int i = 0; i <= projectileCount; i++) { //add +1 to make the pattern symmetrical??
+                                                             //Debug.Log(curAngle);
+                    GameObject go = Instantiate(projectile, transform.position,
+                        Quaternion.Euler(0, transform.rotation.eulerAngles.y + curAngle, 0));
+                    go.GetComponent<Rigidbody>().velocity = go.transform.forward * 5f;
+                    Destroy(go, 5f);
+                    curAngle += angleStep;
+                }
+                yield return Timing.WaitForSeconds(fireRate);
             }
-            yield return Timing.WaitForSeconds(fireRate);
         }
         isFiring = false;
         yield return 0f;
--- Assets/_Script/UI/HealthBar.cs
+++ Assets/_Script/UI/HealthBar.cs
@@ -1,15 +1,55 @@
-﻿using System.Collections;
+﻿using MEC;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour{
     public Image healthBar;
+    public Image lerpedHealthBar; //this healthbar is going to be used to show the effect when taking damage
+    public float lerpSpeed = 7f; //how fast do we lerp
+    public float lerpDelay = .5f; //How soon after taking damage do we lerp
+    private bool _canLerp = false;
+    private CoroutineHandle delayHandle;
+    private float TargetValue {
+        get { return healthBar.fillAmount; }
+    }
+    private float _originalHealth;
 
-    public void UpdateHealthBar(float hp){
+    public void UpdateHealthBar(float newHp){
         //Scales the foreground of the healthbar to the value of hp clamped between 0 and 1
         /*healthBar.transform.localScale = new Vector3(Mathf.Clamp(hp, 0f, 1f), healthBar.transform.localScale.y,
             healthBar.transform.localScale.z);*/
-        healthBar.fillAmount = hp;
+        healthBar.fillAmount = newHp;
+        //if we are alive, do the standard delay then lerp, only resetting when we get hit again
+        if (newHp > 0) {
+            _canLerp = false;
+            Timing.KillCoroutines(delayHandle);
+            delayHandle = Timing.RunCoroutine(SetLerp());
+        }
+        //if we die, ignore everything and fall to 0 quickly
+        else {
+            _canLerp = true;
+            lerpSpeed *= 1.35f;
+        }
+        
+    }
+
+    /// <summary>
+    /// Lerp from original health value to new value
+    /// </summary>
+    public void Update() {
+        if (lerpedHealthBar && _canLerp) {
+            lerpedHealthBar.fillAmount = Mathf.Lerp(lerpedHealthBar.fillAmount, TargetValue, Time.deltaTime * lerpSpeed);
+            if(Mathf.Abs(lerpedHealthBar.fillAmount - TargetValue) <= 0.001f) {
+                _canLerp = false;
+            }
+        }
+    }
+
+    IEnumerator<float> SetLerp() {
+        yield return Timing.WaitForSeconds(lerpDelay);
+        _canLerp = true;
+        yield return 0f;
     }
 }
