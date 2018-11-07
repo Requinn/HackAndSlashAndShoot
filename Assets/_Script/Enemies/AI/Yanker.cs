40ae10834a0e24d2a248e7de331205a51e6f4908
Joel Lee <lee.joel395@gmail.com>
2018-11-06 20:43:20 -0800

    Added prototype for new enemy. Pulls the player in at a certain distance.

---
+++ Assets/_Script/Enemies/AI/Yanker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JLProject;
+using System;
+using MEC;
+using UnityEngine.AI;
+
+public class Yanker : MeleeUnit {
+    public YankerPull pullAbility;
+
+    protected new void Start() {
+        base.Start();
+        Timing.RunCoroutine(AICycle());
+    }
+
+    protected override void Movement() {
+        if (_vision.inRange) {
+            base.Rotate();
+            if (pullAbility.CanCast && Vector3.Distance(transform.position, target.transform.position) > pullAbility.PullRange) {
+                //if (AnimController) AnimController.WalkForward();
+                _NMAgent.Move(transform.forward * speed * Time.deltaTime);
+            }
+            else if (Vector3.Distance(transform.position, target.transform.position) > _vision.maxAttackRange) {
+                //if (AnimController) AnimController.WalkForward();
+                _NMAgent.Move(transform.forward * speed * Time.deltaTime);
+            }
+        }
+        else {
+            //if (AnimController) AnimController.Idle();
+        }
+    }
+
+    private IEnumerator<float> AICycle() {
+        //while we are alive
+        while (CurrentHealth > 0.0f) {
+            if(!target) { yield return 0; }
+            _vision.CanSeeTarget(target.transform);
+            //if we can see the player
+            if (_vision.inRange) {
+                //and our ability is ready and in range
+                if (pullAbility.CanCast && pullAbility.InRange) {
+                    //wait a little
+                    yield return Timing.WaitForSeconds(0.25f);
+                    //if the player is still in range
+                    if (pullAbility.InRange) {
+                        pullAbility.CastPull();
+                        yield return Timing.WaitForSeconds(0.1f);
+                    }else { //else move
+                        Movement();
+                    }
+                }
+                //and they are in the attack range
+                else if (_vision.inAttackCone) {
+                    weapon.Fire();
+                }
+                //move
+                else {
+                    Movement();
+                }
+            }
+            else {
+                Movement();
+            }
+            yield return 0f;
+        }
+
+    }
+}
---
+++ Assets/_Script/Enemies/YankerPull.cs
@@ -0,0 +1,108 @@
+﻿using HutongGames.PlayMaker;
+using JLProject;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YankerPull : MonoBehaviour {
+    [SerializeField]
+    private float _PullRange = 8f;
+    [SerializeField]
+    private float _pullAngle = 90f;
+    [SerializeField]
+    private float _pullStrength = 80f;
+    [SerializeField]
+    private float _coolDown = 15f;
+
+    private float _timeSinceCast = 15f;
+    private bool _isReadytoCast = true;
+    private bool _isInRange = false;
+
+    public bool CanCast { get { return _isReadytoCast; } }
+    public bool InRange { get { return _isInRange; } }
+    public float PullRange { get { return _PullRange; } }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameController.Controller.PlayerReference) {
+            _isInRange = false;
+            CheckReady();
+            if (_isReadytoCast) {
+                CheckRange();
+                if (_isInRange) {
+                    //CastPull();
+                }
+            }
+        }
+        
+	}
+
+    /// <summary>
+    /// Add a force to the player towards this location at pull force
+    /// </summary>
+    public void CastPull() {
+        GameController.Controller.PlayerReference.GetComponent<ImpactReceiver>().AddImpact(transform.position - GameController.Controller.PlayerReference.transform.position, _pullStrength);
+        _timeSinceCast = 0.0f;
+    }
+
+    /// <summary>
+    /// Check if we're on cooldown
+    /// </summary>
+    public void CheckReady() {
+        if(_timeSinceCast >= _coolDown) {
+            _isReadytoCast = true;
+        }else {
+            _timeSinceCast += Time.deltaTime;
+            _isReadytoCast = false;
+        }
+    }
+
+    /// <summary>
+    /// Simplified FoVDetection to check if this ability is ready to cast
+    /// </summary>
+    public void CheckRange() {
+        float distSQ = (GameController.Controller.PlayerReference.transform.position - transform.position).sqrMagnitude;
+        //check for general range
+        if(distSQ < (_PullRange * _PullRange)) {
+            //Then check for angle relative to our forward
+            Vector3 relativePos = transform.InverseTransformPoint(GameController.Controller.PlayerReference.transform.position);
+            Vector3 projected = Vector3.Project(relativePos, Vector3.forward);
+            if (projected.sqrMagnitude > 0.0f) {
+                float angle = Vector3.Angle(GameController.Controller.PlayerReference.transform.position - transform.position, transform.forward);
+                if(angle <= _pullAngle) {
+                    _isInRange = true;
+                }
+            }
+        }
+    }
+}
+
+public class YankerAbilityCheck : FsmStateAction {
+    public FsmBool pullReady = false;
+    public FsmBool pullInRange = false;
+    public FsmOwnerDefault gameObject;
+    private YankerPull pull;
+
+    public override void OnEnter() {
+        pull = Fsm.GetOwnerDefaultTarget(gameObject).GetComponent<YankerPull>();
+        pullReady.Value = pull.CanCast;
+        pullInRange.Value = pull.InRange;
+        Finish();
+    }
+}
+
+public class YankerAbilityAction : FsmStateAction {
+    public FsmOwnerDefault gameObject;
+    private YankerPull pull;
+
+    public override void OnEnter() {
+        pull = Fsm.GetOwnerDefaultTarget(gameObject).GetComponent<YankerPull>();
+        pull.CastPull();
+        Finish();
+    }
+}
